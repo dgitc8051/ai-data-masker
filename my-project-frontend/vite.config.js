a7e8164d1d6f99220fbf2d7fd5585e61a8dd4507
Ian <ian_tsai@intellitrustme.com>
2026-02-02 07:25:18 +0800

    允許所有主機預覽

--- my-project-frontend/vite.config.js
+++ my-project-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,           // 允許外部連線
+    allowedHosts: 'all',  // 允許所有 domain（包含 Railway）
+  },
 })
